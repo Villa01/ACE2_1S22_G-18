b2edcaa5559619864c647d16c37cf9cb25de269b
Erick <javillatoro1@gmail.com>
2022-02-22 14:47:24 -0600

    Manejo de error cuando no hay conexion Serial

--- Practica 1/backend/src/index.js
+++ Practica 1/backend/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 // Puerto a exponer
 const port = 3000;
 
-app.listen(port, ()=> console.log('Servidor escuchando en puerto ', port));
+app.listen(port, () => console.log('Servidor escuchando en puerto ', port));
 
 // Conexion con Arduino
 
@@ -22,11 +22,35 @@ const mySerial = new SerialPort(serialPort, {
     baudRate: baudage
 });
 
-mySerial.open( () => {
-    console.log('Puerto serial abierto');
+mySerial.on('error', function (err) {
+    console.log(err);
 });
 
-const parser = mySerial.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+mySerial.open((err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    const parser = mySerial.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+    parser.on('data', async data => {
+        if (data) {
+            try {
+                const info = JSON.parse(data.toString());
+                info['time'] = new Date().toLocaleString()
+                console.log(info)
+                currentData = info
+                await insertData(info);
+
+            } catch (err) {
+                console.log('ERROR')
+                console.error(err.message)
+            }
+        }
+    });
+
+});
+
 
 let currentData = {
     temp1: 0,
@@ -35,25 +59,9 @@ let currentData = {
     humedad: 0,
     co2: 0,
     time: '00/00/0000, 0:00:00 AM'
-  }
-
-parser.on('data', async data => {
-    if(data){
-        try {
-            const info = JSON.parse(data.toString());
-            info['time'] = new Date().toLocaleString()
-            console.log(info)
-            currentData = info
-            await insertData(info);
-            
-        } catch( err ){
-            console.log('ERROR')
-            console.error( err.message )
-        }
-    }
-});
+}
 
 // Routes
-app.get('/getCurrentData', async(req, res) => {
+app.get('/getCurrentData', async (req, res) => {
     res.json(currentData);
 });
