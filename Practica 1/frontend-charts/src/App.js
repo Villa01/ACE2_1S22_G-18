4ddb716e3050319934253c4b6d32b7b309b6868a
davidlux123 <alexlux94@gmail.com>
2022-02-22 23:20:28 -0600

    commit Deivid

---
+++ Practica 1/frontend-charts/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Temperaturas } from './components/temperaturas';
+
+function App() {
+  return (
+    <div className="App">
+      <Temperaturas/>
+      <Temperaturas/>
+      <Temperaturas/>
+    </div>
+  );
+}
+
+export default App;
