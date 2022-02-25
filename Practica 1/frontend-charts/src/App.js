5e84554a729b2b58bc7bb6417aee2c2592097c43
davidlux123 <alexlux94@gmail.com>
2022-02-24 18:19:17 -0600

    commitDavid

--- Practica 1/frontend-charts/src/App.js
+++ Practica 1/frontend-charts/src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
+import { Barras } from './components/barras';
 import { Temperaturas } from './components/temperaturas';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const GetLogs = async () => {
+    await axios.get('http://localhost:5000/getData')
+      .then((response) => {
+          //console.log(response.data)
+          if (JSON.stringify(data) !== JSON.stringify(response.data)){
+            setData(response.data);
+          }
+      }); 
+  }
+
+  useEffect(()=>{
+    GetLogs();
+  },[data])
+  
   return (
     <div className="App">
-      <Temperaturas/>
-      <Temperaturas/>
-      <Temperaturas/>
+      <h2>Grafica Temperaturas </h2>
+      <div className='padre'>
+        <Temperaturas className="hijo" data = {data} />
+      </div>
+      <div className='padre'>
+        <Barras className="hijo" data = {data} name= "humedad" color = "#30cbf2"/>
+        <Barras className="hijo" data= {data} name= "lumin" color = "#f2de30" />
+      </div>
+      <div className='padre'>
+        <Barras className="hijo" data = {data} name= "co2" color = "#ffffff" />
+      </div>
     </div>
   );
 }
--- Practica 1/frontend-charts/src/components/temperaturas.js
+++ Practica 1/frontend-charts/src/components/temperaturas.js
@@ -1,64 +1,30 @@
 import React from 'react'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-  {
-    name: '1/1/1 00:00',
-    temp1: 40,
-    temp2: 20,
-  },
-  {
-    name: '1/1/1 00:05',
-    temp1: 80,
-    temp2: 10,
-  },
-  {
-    name: '1/1/1 00:10',
-    temp1: 60,
-    temp2: 40,
-  },
-  {
-    name: '1/1/1 00:15',
-    temp1: 90,
-    temp2: 10,
-  },
-  {
-    name: '1/1/1 00:20',
-    temp1: 10,
-    temp2: 80,
-  },
-  {
-    name: '1/1/1 00:25',
-    temp1: 40,
-    temp2: 60,
-  }
-];
-
-export const Temperaturas = () => {
+export const Temperaturas = (props) => {
   return (
     <>
-        <h2>Grafica Temperaturas </h2>
-        <ResponsiveContainer width="100%" aspect={3}>
-            <LineChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="temp1" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="temp2" stroke="#82ca9d" activeDot={{ r: 8 }} />
-            </LineChart>
-        </ResponsiveContainer>
+      <ResponsiveContainer width="80%" aspect={3}>
+          <LineChart
+              width={500}
+              height={300}
+              data={props.data}
+              margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+              }}
+          >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="time" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="temp1" stroke="#9e0606" activeDot={{ r: 8 }} />
+              <Line type="monotone" dataKey="temp2" stroke="#82ca9d" activeDot={{ r: 8 }} />
+          </LineChart>
+      </ResponsiveContainer>
     </>
   )
 }
