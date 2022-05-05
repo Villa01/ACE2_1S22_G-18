da51065e9342bf1e1da1f38cccd13858a51fed35
David Lux <alexlux94@gmail.com>
2022-05-05 13:41:42 -0600

    front

---
+++ Proyecto2/front-end/src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          hola mundo
+      </header>
+    </div>
+  );
+}
+
+export default App;
